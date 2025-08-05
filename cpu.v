e42dac9729d72d5862d40c84b9a253a8479303aa
Akshar B A <122289730+AksharRao@users.noreply.github.com>
2025-08-06 00:21:55 +0530

    dip comment

--- cpu.v
+++ cpu.v
@@ -17,5 +17,31 @@ module cpu(
     input wire rdy                  // Ready signal. Pauses CPU when ready_signal=0
 );
 
+/* This is to understand the DIP layout of the CPU.
+
+          .-------.
+     GND | 1     40 | RES
+     RDY | 2     39 | φ 2 out
+  φ 1 out| 3     38 | S.O.
+     IRQ | 4     37 | φ 0 in
+      NC | 5     36 | NC
+     NMI | 6     35 | NC
+    SYNC | 7     34 | R/W
+     VCC | 8     33 | D0
+      A0 | 9     32 | D1
+      A1 | 10    31 | D2
+      A2 | 11    30 | D3
+      A3 | 12    29 | D4
+      A4 | 13    28 | D5
+      A5 | 14    27 | D6
+      A6 | 15    26 | D7
+      A7 | 16    25 | A15
+      A8 | 17    24 | A14
+      A9 | 18    23 | A13
+     A10 | 19    22 | A12
+     A11 | 20    21 | GND
+         `-------'
+
+*/
 
 endmodule
